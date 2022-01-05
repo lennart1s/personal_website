904e84d69f139d721476a8566a19a79001756873
lennart1s <sandbothe.lennart@gmail.com>
2022-01-05 19:51:28 +0100

    Redid vue setup

--- apps/website/src/App.vue
+++ apps/website/src/App.vue
@@ -1,38 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>The Server says:</p>
-    <p>{{ msg }}</p>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  },
-  data: () => ({
-    msg: '...',
-  }),
-  async mounted() {
-    const resp = await fetch('http://localhost:9090/api/hello');
-    const data = await resp.json();
-    this.msg = data.message;
-  },
-}
-</script>
-
-<style>
+<style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
