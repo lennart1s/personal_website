900b6e2047f6b7d6a73df0c94c2c29328295ae31
lennart1s <sandbothe.lennart@gmail.com>
2022-04-28 13:33:16 +0200

    .

--- apps/api/main.go
+++ apps/api/main.go
@@ -4,13 +4,27 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/api/hello", handler)
-	http.HandleFunc("/api/auth/callback", authCallbackHandler)
+	r := mux.NewRouter()
 
-	http.ListenAndServe(":9090", nil)
+	// http.HandleFunc("/api/hello", handler)
+	// http.HandleFunc("/api/auth/callback", authCallbackHandler)
+	// http.HandleFunc("")
+	r.HandleFunc("/api/hello", handler)
+	r.HandleFunc("/api/users", userPOSTHandler)
+
+	// http.Handle("/", r)
+	http.ListenAndServe(":9090", r)
+}
+
+func userPOSTHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("New User registered")
+	fmt.Printf("%s\n\n", formatRequest(r))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -37,4 +51,42 @@ func authCallbackHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	http.Redirect(w, r, url, 301)
+}
+
+func formatRequest(r *http.Request) string {
+	// Create return string
+	var request []string
+	// Add the request string
+	url := fmt.Sprintf("%v %v %v", r.Method, r.URL, r.Proto)
+	request = append(request, url)
+	// Add the host
+	request = append(request, fmt.Sprintf("Host: %v", r.Host))
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+
+	// If this is a POST, add post data
+	if r.Method == "POST" {
+		r.ParseForm()
+		request = append(request, "\n")
+		request = append(request, r.Form.Encode())
+	}
+
+	var data []byte
+	buffer := make([]byte, 512)
+	for n, err := r.Body.Read(buffer); n > 0; n, err = r.Body.Read(buffer) {
+		data = append(data, buffer[:n]...)
+		if err != nil && err != io.EOF {
+			fmt.Println(err.Error())
+			return "error"
+		}
+	}
+	request = append(request, string(data))
+
+	// Return the request as a string
+	return strings.Join(request, "\n")
 }
